02c1a4f8c6f920c18d4dbefeaa41f927172f5007
Cesar Velasco <cesar.velasco@akimad.com>
2022-08-22 16:29:51 -0500

    :adhesive_bandage: fixed port env error handling

--- configs/env.go
+++ configs/env.go
@@ -19,7 +19,9 @@ func EnvMongoURI() string {
 func EnvPort() string {
 	err := godotenv.Load()
 	if err != nil {
+
 		log.Fatal("Error loading .env file")
+		return "3000"
 	}
 
 	return os.Getenv("PORT")
