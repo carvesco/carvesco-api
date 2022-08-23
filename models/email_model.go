90e0f84d1f2aa398ce63779e575a93bd348c9f94
Cesar Velasco <cesar.velasco@akimad.com>
2022-08-23 14:44:22 -0500

    :adhesive_bandage: simple email model change

--- models/email_model.go
+++ models/email_model.go
@@ -5,6 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Email struct {
 	Id          primitive.ObjectID `json:"id,omitempty"`
 	FromAddress string             `json:"fromaddress,omitempty" validate:"required"`
+	Name        string             `json:"name,omitempty" validate:"required"`
 	ToAddress   string             `json:"toaddress,omitempty" validate:"required"`
 	Message     string             `json:"message,omitempty" validate:"required"`
 }
